366cb7fa089b3984d665814e0070b191f6160293
DinhThanhRN <hinhtudien@gmail.com>
2023-05-31 23:43:53 +0700

    Remove request limiter

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Limit requests from same API
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many request from this id. Please try again in an hour!",
-});
-app.use("/api", limiter);
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many request from this id. Please try again in an hour!",
+// });
+// app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
